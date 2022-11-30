3b151cc71564a816f17fc92b9e0a3feffbfdac49
juuliadidenko <hiljaotsola2766@gmail.com>
2022-12-01 02:51:39 +0300

    Add JWT dependency

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
+
+
+load_dotenv()
 
 
 app = Flask(__name__)
 api = Api(app)
+app.secret_key = os.getenv('SECRET_KEY')
 
+jwt = JWT(app, authenticate, identity)
 
 items = []
 
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
         return {'item': item}, 200 if item else 404
