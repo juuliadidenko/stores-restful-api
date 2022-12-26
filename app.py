b59d01e9a0501ab0080f47ed7b5fe8509481bff1
Julia <hiljaotsola2766@gmail.com>
2022-12-27 01:19:53 +0300

    refactor item and user models with flask-sqlalchemy syntax

--- app.py
+++ app.py
@@ -2,26 +2,48 @@ import os
 from dotenv import load_dotenv
 from flask import Flask
 from flask_restful import Api
-from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
-from recources.user import UserRegister, UserLogin
-from recources.item import Item, ItemList
+from resources.user import UserRegister, UserLogin
+from resources.item import Item, ItemList
+from db import db
+from models.item import ItemModel
+from models.user import UserModel
 
 
-load_dotenv()
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+
+
+
 
 
 app = Flask(__name__)
 api = Api(app)
-app.secret_key = os.getenv('SECRET_KEY')
-db = SQLAlchemy()
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 jwt = JWTManager(app)
+
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' \
+    + os.path.join(basedir, 'data.db')
+db.init_app(app)
 
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 api.add_resource(UserLogin, '/login')
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        'db': db,
+        'UserModel': UserModel,
+        'ItemModel': ItemModel
+    }
 
 
 if __name__ == "__main__":
--- resources/item.py
+++ resources/item.py
@@ -1,4 +1,3 @@
-import sqlite3
 from flask_restful import Resource, reqparse
 from flask_jwt_extended import jwt_required
 from models.item import ItemModel
@@ -23,12 +22,12 @@ class Item(Resource):
     def post(self, name):
         if ItemModel.find_by_name(name):
             return {
-                "message": f'An item with name {name} already exists'}
+                "message": f"An item with name {name} already exists"}
         data = Item.parser.parse_args()
         item = ItemModel(name, data['price'])
         
         try:
-            item.insert()
+            item.save_to_db()
         except:
             return {
                 "message": "An error occurred while inserting the item"
@@ -39,48 +38,22 @@ class Item(Resource):
     def put(self, name):
         data = Item.parser.parse_args()
         item = ItemModel.find_by_name(name)
-        updated_item = ItemModel(name, data['price'])
 
         if item is None:
-            try:
-                updated_item.insert()
-            except:
-                return {
-                    "message": "An error occurred while inserting the item"
-                    }, 500
+            item = ItemModel(name, data['price'])
         else:
-            try:
-                updated_item.update()
-            except:
-                return {
-                    "message": "An error occurred while updating the item"
-                    }, 500
-        return updated_item.json()
+            item.price = data['price']
+        item.save_to_db()
+        return item.json()
 
     def delete(self, name):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        query = 'DELETE FROM items WHERE name=?'
-        cursor.execute(query, (name,))
-        connection.commit()
-        connection.close()
-        return {'message': 'Item deleted'}
+        item = ItemModel.find_by_name(name)
+        if item:
+            item.delete_from_db()
+        return {"message": "Item deleted"}
 
 
 class ItemList(Resource):
     def get(self):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        query = 'SELECT * FROM items'
-        result = cursor.execute(query)
-        items = []
-        for row in result:
-            items.append(
-                {
-                    "name": row[0],
-                    "price": row[1]
-                }
-            )
-
-        connection.close()
-        return {"items": items}
+        items = ItemModel.query.all()
+        return {"items": [item.json() for item in items]}
